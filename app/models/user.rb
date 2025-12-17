fc34f9187a56b3b404f6c47317a48bb5ce155109
Ismat-Samadov <ismetsemedov@gmail.com>
2025-12-17 14:15:50 +0400

    init

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence:true
-  validates :password, presence:true, length:{minimum: 5}
-end
